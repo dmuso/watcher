674e2559e358d032747f528a49e27a63ef0257bc
Dan Harper <dan@kingdomsolutions.com.au>
2021-09-26 12:55:50 +1000

    Set max volume only if volume above the max

--- watcher.sh
+++ watcher.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-pkill -9 "launcher"
-pkill -9 "launcher-Helper"
-pkill -9 "java"
-pkill -9 Skype
+# pkill -9 "launcher"
+# pkill -9 "launcher-Helper"
+# pkill -9 "java"
+# pkill -9 Skype
+osascript -e "if output volume of (get volume settings) > 70 then 
+set volume output volume 70 
+end if"
